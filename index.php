b10958c9c32f9391c6287a6703a719681f04c14e
Anthony Gabriel Neves Mendes <anthony.gabriel@dinfor.uniceub>
2025-03-24 10:40:39 -0300

    sql

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    include("conexao.php");
+
+    $sql = "select * from aluno";
+    $result = $conexao->query($sql);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <table border="1" width="100%">
+        <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>RA</th>
+                <th>EMAIL</th>
+        </tr>
+        <?php while ($row = $result->fetch(PDO::FETCH_OBJ)) { ?>
+            <tr>
+                <td><?php echo $row->id ?></td>
+                <td><?php echo $row->NOME ?></td>
+                <td><?php echo $row->ra ?></td>
+                <td><?php echo $row->id ?></td>
+            </tr>
+        <?php } ?>
+        </table>
+</body>
+</html>
