e622222490bd7ebc4f356e550219b466383f18b3
Anthony Gabriel Neves Mendes <anthony.gabriel@dinfor.uniceub>
2025-04-07 08:32:22 -0300

    Registro

--- inserir.php
+++ inserir.php
@@ -2,16 +2,25 @@
 
     include_once("conexao.php");
 
+    $id =isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
     $nome= $_REQUEST['Nome'];
     $ra = $_REQUEST['ra'];
     $email = $_REQUEST['email'];
     
-    $sql ="INSERT INTO  aluno (nome, ra, email)
+    if($id) {
+
+    } else {
+        //realiza a inserção de um novo registro
+        $sql ="INSERT INTO  aluno (nome, ra, email)
             VALUES (:nome, :ra, :email)";
      $stmt = $conexao->prepare($sql);
      $stmt->bindParam(':nome', $nome);
      $stmt->bindParam(':ra',$ra);
      $stmt->bindParam(':email', $email);
+
+    }
+
+    
      $stmt->execute();
 
      header("Location: index.php");
